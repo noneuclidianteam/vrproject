5bf7e5810a7ac8a9c1dcb61d1d9156921fe791c2
Grégoire Chamberland <gregoire.chamberland@gmail.com>
2017-11-28 11:20:32 +0100

    Portal teleportation.

---
+++ Assets/Script/FirstPersonController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour {
+
+	public Camera Camera;
+
+	public float WalkSpeed, RunSpeed;
+	public float CameraSpeed;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float v = Input.GetAxis ("Vertical");
+		float h = Input.GetAxis ("Horizontal");
+
+		bool isRunning = Input.GetAxis ("Run") > 0;
+
+		float speed = isRunning ? RunSpeed : WalkSpeed;
+
+		this.transform.position += speed * v * transform.forward * Time.deltaTime;
+		this.transform.position += speed * h * Vector3.Cross (transform.forward, transform.up) * Time.deltaTime;
+
+		float cameraH = Input.GetAxis ("Mouse X");
+		float cameraV = -Input.GetAxis ("Mouse Y");
+		this.transform.Rotate (0f, CameraSpeed * cameraH, 0f);
+		Camera.transform.Rotate (CameraSpeed * cameraV, 0f, 0f);
+	}
+}
---
+++ Assets/Script/Portal.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+	public Camera MainCamera;
+	public int SourceLayer, DestinationLayer;
+
+	private bool crossed = false;
+	private Camera renderCamera;
+	private MeshRenderer renderer;
+
+	private void enableLayer(Camera cam, int layer) {
+		cam.cullingMask = cam.cullingMask | 1 << layer;
+	}
+
+	private void disableLayer(Camera cam, int layer) {
+		cam.cullingMask = cam.cullingMask & ~(1 << layer);
+	}
+
+	// Use this for initialization
+	void Start () {
+		renderCamera = (Camera) Camera.Instantiate(
+			MainCamera.GetComponent<Camera>(),
+			MainCamera.transform.position,
+			MainCamera.transform.rotation,
+			MainCamera.transform
+		);
+
+		renderCamera.tag = "Untagged";
+
+		enableLayer (renderCamera, DestinationLayer);
+		disableLayer (renderCamera, SourceLayer);
+
+		enableLayer (MainCamera, SourceLayer);
+		disableLayer (MainCamera, DestinationLayer);
+
+		renderCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+		Material mat = new Material(Shader.Find("Hidden/PortalEffectShader"));
+		renderer = GetComponent<MeshRenderer> ();
+		renderer.material = mat;
+		mat.mainTexture = renderCamera.targetTexture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider collider) {
+
+		if (!collider.gameObject.CompareTag (MainCamera.tag)) {
+			return;
+		}
+
+		Vector3 camToPortal = this.transform.position - MainCamera.transform.position;
+
+		if (crossed) {
+			disableLayer (MainCamera, DestinationLayer);
+			enableLayer (MainCamera, SourceLayer);
+
+			disableLayer (renderCamera, SourceLayer);
+			enableLayer (renderCamera, DestinationLayer);
+
+			this.gameObject.layer = SourceLayer;
+		} else {
+			disableLayer (MainCamera, SourceLayer);
+			enableLayer (MainCamera, DestinationLayer);
+
+			disableLayer (renderCamera, DestinationLayer);
+			enableLayer (renderCamera, SourceLayer);
+
+			this.gameObject.layer = DestinationLayer;
+		}
+
+		this.gameObject.transform.Rotate (0f, 180f, 0f, Space.World);
+			
+		crossed = !crossed;
+	}
+}
