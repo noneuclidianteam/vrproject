0e5d249423e409f2ed7d80ed8e64b83e0098d108
clement.mezelle@free.fr <clement.mezelle@free.fr>
2017-11-30 15:26:27 +0100

    InfinitePIllar V.1

---
+++ Assets/Script/PillarChangeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarChangeScript : MonoBehaviour
+{
+    public GameObject currentPillar;
+
+    public GameObject nextPillar;
+
+    private bool isAltPillarActive = false;
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+       // print("hasToEnablePillar = " + PillarDetectionScript.hasToEnablePillar);
+
+        if (!currentPillar.activeInHierarchy && !nextPillar.activeInHierarchy)
+            return;
+
+        if (PillarDetectionScript.hasToEnablePillar)
+        {
+           // print("Ready to enable");
+
+            currentPillar.SetActive(isAltPillarActive);
+            nextPillar.SetActive(!isAltPillarActive);
+
+            isAltPillarActive = !isAltPillarActive;
+            PillarDetectionScript.hasToEnablePillar = false;
+        }
+    }
+    
+
+}
---
+++ Assets/Script/PillarDetectionScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarDetectionScript : MonoBehaviour
+{
+
+    public static bool hasToEnablePillar;
+    
+    private Camera mainCamera;
+    private Vector3 lastCamPos; 
+
+    void Start ()
+    {
+        mainCamera = Camera.main;
+        hasToEnablePillar = false;
+	}
+	
+
+	void Update ()
+    {
+        lastCamPos = mainCamera.transform.position;
+    }
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        //if (!collider.gameObject.CompareTag(mainCamera.tag))
+        //   return;
+
+        Vector3 currCamPos = mainCamera.transform.position;
+
+        print(Vector3.Dot(currCamPos - lastCamPos, transform.forward));
+
+        hasToEnablePillar = Vector3.Dot(currCamPos - lastCamPos, transform.forward) < 0f;
+    }
+}
+
+
+enum directionVector
+{
+    Forward,
+
+}
