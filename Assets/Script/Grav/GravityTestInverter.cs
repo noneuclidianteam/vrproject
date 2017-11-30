660edaef3a216572269a55be21f48523d0c7f19b
FMorgand <franckmorgand@gmail.com>
2017-11-30 15:57:43 +0100

    Added grav Inverter test

---
+++ Assets/Script/Grav/GravityTestInverter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityTestInverter : MonoBehaviour {
+
+	public GameObject Normal;
+
+	public GameObject Innverted;
+
+	bool IsInverted =false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider ObjectCol)
+	{
+		if (ObjectCol.tag == "InverterPortal") 
+		{
+			if (IsInverted == false) {
+				Normal.SetActive (false);
+				Innverted.SetActive (true);
+			} else {
+				Normal.SetActive (true);
+				Innverted.SetActive (false);
+			}
+
+			IsInverted = !IsInverted;
+		}
+	}
+}
