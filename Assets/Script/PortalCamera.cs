6a6a67c8fc8b8bf407435fbd725d0740115b8bfb
Grégoire Chamberland <gregoire.chamberland@gmail.com>
2017-12-01 16:22:08 +0100

    Test portal VR

---
+++ Assets/Script/PortalCamera.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.XR;
+
+public class PortalCamera : MonoBehaviour {
+
+	public int textureSize = 1024;
+
+	public Camera _vrEye;
+
+	private Camera _cameraForPortal;
+	private RenderTexture _leftEyeRenderTexture;
+	private RenderTexture _rightEyeRenderTexture;
+
+	private void Awake() { 
+		//_vrEye = SteamVR.instance.
+		_cameraForPortal = GetComponent<Camera>();
+		_cameraForPortal.enabled = false;
+		_leftEyeRenderTexture = new RenderTexture(textureSize, textureSize, 24);
+		_rightEyeRenderTexture = new RenderTexture(textureSize, textureSize, 24);
+	}
+
+	public void RenderIntoMaterial(Material material) {
+		if (Camera.current == _vrEye) {
+			transform.localRotation = _vrEye.transform.localRotation; // left eye
+			Vector3 eyeOffset = SteamVR.instance.eyes[0].pos;
+			transform.localPosition = _vrEye.transform.position + _vrEye.transform.TransformVector(eyeOffset);
+			_cameraForPortal.projectionMatrix = HMDMatrix4x4ToMatrix4x4(
+				SteamVR.instance.hmd.GetProjectionMatrix(
+					Valve.VR.EVREye.Eye_Left,
+					_vrEye.nearClipPlane,
+					_vrEye.farClipPlane
+				)
+			);
+			_cameraForPortal.targetTexture = _leftEyeRenderTexture;
+			_cameraForPortal.Render();
+
+
+			material.SetTexture("_LeftEyeTexture", _leftEyeRenderTexture); // right eye
+			eyeOffset = SteamVR.instance.eyes[1].pos;
+			transform.localPosition = _vrEye.transform.position + _vrEye.transform.TransformVector(eyeOffset);
+			_cameraForPortal.projectionMatrix = HMDMatrix4x4ToMatrix4x4(
+				SteamVR.instance.hmd.GetProjectionMatrix(
+					Valve.VR.EVREye.Eye_Right,
+					_vrEye.nearClipPlane,
+					_vrEye.farClipPlane
+				)
+			);
+			_cameraForPortal.targetTexture = _rightEyeRenderTexture;
+			_cameraForPortal.Render(); material.SetTexture("_RightEyeTexture", _rightEyeRenderTexture);
+		} 
+	}
+
+	private Matrix4x4 HMDMatrix4x4ToMatrix4x4(Valve.VR.HmdMatrix44_t input) {
+		var m = Matrix4x4.identity;
+		m[0, 0] = input.m0;
+		m[0, 1] = input.m1;
+		m[0, 2] = input.m2;
+		m[0, 3] = input.m3;
+		m[1, 0] = input.m4;
+		m[1, 1] = input.m5;
+		m[1, 2] = input.m6;
+		m[1, 3] = input.m7;
+		m[2, 0] = input.m8;
+		m[2, 1] = input.m9;
+		m[2, 2] = input.m10;
+		m[2, 3] = input.m11;
+		m[3, 0] = input.m12;
+		m[3, 1] = input.m13;
+		m[3, 2] = input.m14;
+		m[3, 3] = input.m15;
+		return m; 
+	}
+}
---
+++ Assets/Shader/VRPortalShader.shader
@@ -0,0 +1,47 @@
+﻿Shader "Custom/portal_shader" {
+	Properties {
+		_LeftTex ("Albedo (RGB)", 2D) = "white" {}
+		_RightTex ("Albedo (RGB)", 2D) = "white" {}
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Standard fullforwardshadows
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _LeftTex;
+		sampler2D _RightTex;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		// Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+		// See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+		// #pragma instancing_options assumeuniformscaling
+		UNITY_INSTANCING_CBUFFER_START(Props)
+			// put more per-instance properties here
+		UNITY_INSTANCING_CBUFFER_END
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			// Metallic and smoothness come from slider variables
+			//if (unity_StereoEyeIndex == 0)
+			if (unity_CameraProjection[0][2] < 0) {
+				float4 c = tex2D(_LeftTex, IN.uv_MainTex);
+				o.Albedo = c.rgb;
+				o.Alpha = c.a;
+			} else {
+				float4 c = tex2D(_RightTex, IN.uv_MainTex);
+				o.Albedo = c.rgb;
+				o.Alpha = c.a;
+			}
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
