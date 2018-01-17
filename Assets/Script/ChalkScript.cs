d2a7dae64fc4091bfc09d32ca1f5dbfdf7634614
Phenix8 <clement.mezelle@free.fr>
2018-01-17 10:38:11 +0100

    1er Tests pour la craie
    Utilisation d'un LineRenderer

---
+++ Assets/Script/ChalkScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChalkScript : MonoBehaviour {
+
+    Vector3 previousPos;
+
+    [Range(0.001f, 0.01f)]
+    public float detectionTolerance;
+
+    public LineRenderer lr;
+
+
+	void Start ()
+    {
+        previousPos = transform.position;
+        lr.SetPositions(
+            new Vector3[] {
+                transform.position,
+                transform.position
+            });
+	}
+	
+
+	void Update () {
+
+        Vector3 diffPos = transform.position - previousPos;
+        if (diffPos.x > detectionTolerance || diffPos.y > detectionTolerance || diffPos.z > detectionTolerance)
+        {
+            print("Updating line points.");
+            lr.positionCount++;
+            lr.SetPosition(lr.positionCount - 1, transform.position);
+        }
+
+	}
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        
+    }
+
+}
