4a36d1a323d94cb22604d5f89475c7d3653b555b
Grégoire Chamberland <gregoire.chamberland@gmail.com>
2018-01-18 19:02:04 +0100

    Portals visible though portals + optimizations + portals are not toggled anymore when crossed.

---
+++ Assets/Script/Room.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+	private List<Portal> portals;
+
+	public List<Portal> getPortals() {
+		return portals;
+	}
+
+	// Use this for initialization
+	void Start () {
+		portals = new List<Portal> (GetComponentsInChildren<Portal> ());
+
+		foreach (Portal portal in portals) {
+			portal.setRoom (this);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
